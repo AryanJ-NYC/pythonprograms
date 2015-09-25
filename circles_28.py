becb0314f681639e152fbf7a1ee4a3f76dca80ae
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-09-25 11:00:04 -0400

    Added original copies

--- circles_28.py
+++ circles_28.py
@@ -1,157 +1,107 @@
-# circles_28 - NEXT
-# FOR 13 VARIABLES of map2_4.py
+#circles_28 - NEXT
+#FOR 13 VARIABLES of map2_4.py
 
 import math
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 import random
 import time
 
-outer_index = -2  # Must be -2 since 'i' is not incremented at first run of animate
 
-def perp(vxin, vyin):
-    if vxin == 0:
-        vxp = -np.sign(vyin) * vyin
-        vyp = 0.
+def perp(vxin,vyin):
+    if (vxin==0):
+        vxp=-np.sign(vyin)*vyin
+        vyp=0.
     else:
-        vyp = np.sign(vxin) * 1.0 / np.sqrt(1 + vyin ** 2 / vxin ** 2)
-        vxp = -np.sign(vyin) * abs(vyin * vyp / vxin)
-    return vxp, vyp
+        vyp=np.sign(vxin)*1.0/np.sqrt(1+vyin**2/vxin**2)
+        vxp=-np.sign(vyin)*abs(vyin*vyp/vxin)
+    return vxp,vyp
+    
+#-------------------------------------------------------------------------
 
+#NEED TO CHANGE THIS LATER
+# numc=8
+# cin=[[0, 3, -6, 3, 3, 3, 0, 0], [3, 0, 0, 0, 0, 0, 0, 3], [-3, 0, 0, 0, 0, 0, 0, -3], [3, 0, 0, 0, 0, 0, 0, 0], [3, 0, 0, 0, 0, 0, 0, 0], [3, 0, 0, 0, 0, 0, 3, 0], [0, 0, 0, 0, 0, 3, 0, 0], [0, 3, -6, 0, 0, 0, 0, 0]]
+# zin=[13.31636449,   6.65819304,   0.        ,   3.33333333,
+#          3.33333333,   6.65819304,   3.33332235,   3.33332235]
 
-# -------------------------------------------------------------------------
-
-# NEED TO CHANGE THIS LATER
-# numc = 8
-# cin = [[0, 3, -6, 3, 3, 3, 0, 0], [3, 0, 0, 0, 0, 0, 0, 3], [-3, 0, 0, 0, 0, 0, 0, -3], [3, 0, 0, 0, 0, 0, 0, 0], [3, 0, 0, 0, 0, 0, 0, 0], [3, 0, 0, 0, 0, 0, 3, 0], [0, 0, 0, 0, 0, 3, 0, 0], [0, 3, -6, 0, 0, 0, 0, 0]]
-# zin = [13.31636449,   6.65819304,   0.,   3.33333333, 3.33333333, 6.65819304, 3.33332235, 3.33332235]
-
-
-def clockplot(cin, zin, programnamein):
-    # set up only for THESE names
-    vname = ['1-NODAL (+/-)',
-             '2-POSITIVE memory',
-             '3-NEGATIVE memory',
-             '4-POSITIVE expect',
-             '5-NEGATIVE expect',
-             '6-Coop/Compete',
-             '7-Conlf Manag',
-             '8-Fairness',
-             '9-Access',
-             '10-Security',
-             '11-Higher Authority',
-             '12-Peace Vision',
-             '13-Shared Ident'
-             ]
-    numc = len(cin)
-    if numc > 13:
+def clockplot(cin,zin,programnamein):
+    #set up only for THESE names
+    vname=['1-NODAL (+/-)','2-POSITIVE memory','3-NEGATIVE memory','4-POSITIVE expect','5-NEGATIVE expect','6-Coop/Compete','7-Conlf Manag','8-Fairness','9-Access','10-Security','11-Higher Authority','12-Peace Vision','13-Shared Ident']
+    numc=len(cin)
+    if numc>13:
         print('\nSORRY, I cannot make a clockplot for more than 13 variables')
         return
-
-    # plot the variables
-    radius = 1.
-    pi = np.pi
-    xp = []
-    yp = []
+    else:
+        pass
+    #
+    
+    #plot the variables
+    radius=1.
+    pi=np.pi
+    xp=['0' for i in range(numc)]
+    yp=['0' for i in range(numc)]
     for i in range(numc):
-        angle = i * pi / 6.5
-        xp.append(radius * np.sin(angle))
-        yp.append(radius * np.cos(angle))
-    xp_array = np.array(xp)
-    yp_array = np.array(yp)
+        angle=i*pi/(6.5)
+        xp[i]=radius*np.sin(angle)
+        yp[i]=radius*np.cos(angle)
+    xpa=np.array(xp)
+    ypa=np.array(yp)
     fig = plt.figure()
-    # plt.axes([.1,.1,.7,.7])
+    #plt.axes([.1,.1,.7,.7])
     ax = fig.add_subplot(111)
     ax.set_aspect('equal')
     fig.patch.set_facecolor('white')
     ax.set_axis_bgcolor('white')
-
+    #ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
     for i in range(numc):
-        # Creates circles (or square)
-        maxz = max(np.abs(zin))
-        msz = abs(zin[i]) * 40 / maxz
-        if msz < 2:
-            msz = 2.
-        symbol = 'o'
-        if i == 0:
-            symbol = 's'
-        varc = 'k'
-        if zin[i] < 0:
-            varc = 'r'
-        elif zin[i] > 0:
-            varc = 'g'
-        # else:
-        #             varc='g'
-        ax.plot(xp_array[i], yp_array[i], symbol, ms=msz, c=varc, markeredgewidth=2.0,
-                markerfacecolor='none', markeredgecolor=varc)
-        if i != 0:
-            ax.text(xp_array[i] + .1, yp_array[i] - 0.07, vname[i])
+        maxz=max(np.abs(zin))
+        msz=abs(zin[i])*40/maxz
+        if msz<2: msz=2.
+        symbol='o'
+        if (i==0): symbol='s'
+        varc='k'
+        if zin[i] < 0: varc='r'
+        if zin[i] > 0: varc='g'
+#         else:
+#             varc='g'
+        plt.plot(xpa[i],ypa[i],symbol,ms=msz,c=varc,markeredgewidth=2.0,\
+    markerfacecolor='none',markeredgecolor=varc)
+        if(i!=0):
+            plt.text(xpa[i]+.1,ypa[i]-0.07,vname[i])
         else:
-            ax.text(xp_array[i] + .2, yp_array[i] + 0.07, vname[i])
-    ax.axis([-1.25, 1.25, -1.25, 1.25])
-    ax.axis('off')
-
-    # plot the connections
-    cina = np.array(cin)
-    counter = 0
-    for i in range(2):
-        for j in range(2):
-            pass
-            # if np.abs(cina[j][i]) > .1:
-            #     width = abs(cina[j][i]) / 2.
-            #     # print ('\nwidth= ',width)
-            #     dxp = (xp_array[j] - xp_array[i]) * 0.9
-            #     dyp = (yp_array[j] - yp_array[i]) * 0.9
-            #     dxshift, dyshift = perp(dxp, dyp)
-            #     if cina[j][i] < 0:
-            #         arrow_color = 'r'
-            #     else:
-            #         arrow_color = 'g'
-            #     roff = 0.03
-            #     xoff = dxshift * roff
-            #     yoff = dyshift * roff
-            #     xpastart = xp_array[i] + xoff
-            #     ypastart = yp_array[i] + yoff
-            #     # print ('\nijxy= ',i,j,xpastart, ypastart)
-            #     ax.arrow(xpastart, ypastart, dxp, dyp, head_width=0.05,
-            #              head_length=0.1, fc=arrow_color, ec=arrow_color, linewidth=width)
-                #     localtime = time.asctime( time.localtime(time.time()) )
-                #     programname='peace_20.py   '+localtime
-    pname = programnamein
+            plt.text(xpa[i]+.2,ypa[i]+0.07,vname[i])
+    plt.axis([-1.25, 1.25, -1.25, 1.25])
+    plt.axis('off')
+    
+    #plot the connections
+    cina=np.array(cin)
+    xcon=['0','0']
+    ycon=['0','0']
+    for i in range(numc):
+            for j in range(numc):
+                if (np.abs(cina[j][i])>.1):
+                    width=str(abs(cina[j][i])/2.)
+                    #print ('\nwidth= ',width)
+                    dxp=(xpa[j]-xpa[i])*0.9
+                    dyp=(ypa[j]-ypa[i])*0.9
+                    dxshift,dyshift=perp(dxp,dyp)
+                    if (cina[j][i] < 0):
+                        test='r'
+                    else:
+                        test='g'
+                    roff=0.03
+                    xoff=dxshift*roff
+                    yoff=dyshift*roff
+                    xpastart=xpa[i]+xoff
+                    ypastart=ypa[i]+yoff
+                    #print ('\nijxy= ',i,j,xpastart, ypastart)
+    
+                    ax.arrow(xpastart,ypastart,dxp, dyp, head_width=0.05,
+    head_length=0.1, fc=test, ec=test,linewidth=width)
+#     localtime = time.asctime( time.localtime(time.time()) )
+#     programname='peace_20.py   '+localtime
+    pname=programnamein
     plt.title(pname, fontsize=12)
-    anim = animation.FuncAnimation(fig, animate, frames=numc**2, fargs=(ax, cin, xp_array, yp_array), interval=1,
-                                   blit=False, repeat=False)
-    plt.show()
-
-counter = 0
-def animate(i, ax, cin, xp_array, yp_array):
-    global outer_index, counter
-    inner_index = i % len(cin)
-
-    if i % 13 == 0:
-        outer_index += 1
-        print("i: {}".format(i))
-    print("Outer index: {}, inner index: {}, i: {}".format(outer_index, inner_index, i))
-    cina = np.array(cin)
-    if np.abs(cina[outer_index][inner_index]) > .1:
-        width = abs(cina[outer_index][inner_index]) / 2.
-        # print ('\nwidth= ',width)
-        dxp = (xp_array[outer_index] - xp_array[inner_index]) * 0.9
-        dyp = (yp_array[outer_index] - yp_array[inner_index]) * 0.9
-        dxshift, dyshift = perp(dxp, dyp)
-        if cina[outer_index][inner_index] < 0:
-            arrow_color = 'r'
-        else:
-            arrow_color = 'g'
-        roff = 0.03
-        xoff = dxshift * roff
-        yoff = dyshift * roff
-        xpastart = xp_array[inner_index] + xoff
-        ypastart = yp_array[inner_index] + yoff
-        # print ('\nijxy= ',i,j,xpastart, ypastart)
-        ax.arrow(xpastart, ypastart, dxp, dyp, head_width=0.05,
-                 head_length=0.1, fc=arrow_color, ec=arrow_color, linewidth=width)
-        print("{}".format(counter))
-        counter += 1
+    return
--- map2_6boxes.py
+++ map2_6boxes.py
@@ -1,235 +1,261 @@
+#map2_5boxes.py
+#starting Peter's 2015-07-18 MAP with 13 variables
+#DOES PETERS DISPLAY
+
+#***NB***
+#REPLACED NODAL,+/- RESERVOIRS, +/- FUTURE AS VARIABLES 1,2,3,4
+#AND +/- RESERVOIRS, +/- FUTURE ==0 IF <0
+#***NB***
+
+#STARTING TO USE BETTER PLOT/COLORS FROM peace_11
+#note: c(i,j) is effect of j on i
+
+
+
 import math
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from scipy import stats
 import numpy as np
 import random
 import time
 from boxes_4 import *
 
+def filein (fname):
+    numlines=0
+    xin=[]
+    f=open(fname,'r')
+    for line in f:
+        #print (line, end='')
+        xin.append(line)
+        numlines=numlines+1
+    f.close()
+    return xin,numlines
 
-def filein(filename):
-    """Reads in a file
-    :param filename: File to be read
-    """
-    xin = open(filename).read().splitlines()
-    numlines = len(xin)
-    return xin, numlines
-
-
-def fileout(filename, filedata):
-    """Writes to file
-    :param filename: File to write to
-    :param filedata: Data to be written to file
-    """
-    f2 = open(filename, 'w')
+def fileout (filename,filedata):
+    f2=open(filename,'w')
     f2.write(filedata)
     f2.close()
 
-
-def getxy(filename):
-    data, numlines = filein(filename)
-    x = []  # '0' for i in range(numlines)]
-    y = []  # '0' for i in range(numlines)]
+def getxy (fname):
+    data,numlines=filein(fname)
+#    dataline=['0' for i in range(numlines)]
+    x=['0' for i in range(numlines)]
+    y=['0' for i in range(numlines)]
     for i in range(numlines):
-        xline = data[i]
-        xline2 = xline.split('\t')
-        xline2[-1] = xline2[-1].replace('\n', '')
+        xline=data[i]
+        xline2=xline.split('\t')
+        xline2[-1]=xline2[-1].replace('\n','')
+        #print ('\nxline2',xline2)
+        x[i]=eval(xline2[0])
+        y[i]=eval(xline2[1])
+    return x,y,numlines
 
-        x.append(eval(xline2[0]))  # [i] = eval(xline2[0])
-        y.append(eval(xline2[1]))  # [i] = eval(xline2[1])
-    return x, y, numlines
-
-
-def getx(filename):
-    """TODO
-    Insert Dr. Liebovitch's doctring here
-    :param filename: Filename to getx
-    """
-    data, numlines = filein(filename)
-    x = []  # x=['0' for i in range(numlines)]
+def getx (fname):
+    data,numlines=filein(fname)
+    #print ('\ndata\n',data,'\nlines',numlines)
+    x=['0' for i in range(numlines)]
     for i in range(numlines):
-        data[i] = data[i].replace('\n', '')
-        x.append(eval(data[i]))
-    return x, numlines
+        x[i]=data[i].replace('\n','')
+        x[i]=eval(x[i])
+    return x,numlines
 
-
-def getxn(filename):
-    data, numlines = filein(filename)
-    dataline = []  # '0' for i in range(numlines)]
+def getxn(fname):
+    data,numlines=filein(fname)
+#print (numlines)
+#print (data)
+    dataline=['0' for i in range(numlines)]
     for i in range(numlines):
-        y = data[i].split('\t')
-        y[-1] = y[-1].replace('\n', '')
-        dataline.append(y)
-
-    xdata = dataline[:]
-
-    for i in range(numlines):
-        line_length = len(dataline[i])
-        for j in range(line_length):
+        x=data[i]
+        y=x.split('\t')
+        y[-1]=y[-1].replace('\n','')
+        dataline[i]=y
+    #print ('\n\nascii-input',dataline)
+    xdata=dataline[:]
+    for i in range (numlines):
+        inline=len(dataline[i])
+        for j in range (inline):
             if xdata[i][j] != '':
-                xdata[i][j] = eval(xdata[i][j])
+                xdata[i][j]=eval(xdata[i][j])
             else:
-                xdata[i][j] = None
-
+                xdata[i][j]=None
+    #print ('dataline',dataline)
+    #print ('xdata',xdata)
     return xdata, numlines
 
-
-def lslin(invars, invar):
-    print('\ncurrent value of {} is {}'.format(invars, invar))
-    outvars = input('\nchange to (def=no change)')
-    if outvars == '':
+#-------------------------------------------------------------------------
+def lslin(invars,invar):
+    print('\ncurrent value of ',invars,' is= ',invar)
+    outvars=input('\nchange to (def=no change)')
+    if (outvars==''):
         return invar
     else:
-        return eval(outvars)
+        outvar=eval(outvars)
+    return outvar
+    
+# def lslina(invars,invar):
+#     print('\ncurrent value of ',invars,' is= ',invar)
+#     outvars=input('\nchange to (def=no change)')
+#     if (outvars==''):
+#         return invar
+#     else:
+#         outvar=eval(outvars)
+#     return outvar
 
+#-------------------------------------------------------------------------
+#integration default parameters
+#NOTE: dt=.001
+dt=.001
+tt=0.
+numdata=30000
+paramin='used data from files'
 
-def generate_filenames():
-    """Generates file names from user's digit input
-    :return: tuple of four file names
-    """
-    # give it just a number n, will find files cn.txt, bn.txt, mn.txt, icn.txt
-    fast = input('\nONLY NUMBER n and I will find cn.txt, etc. (#/a, Def=a)')
-    if fast.isdigit():
-        file_name_c = 'c' + fast + '.txt'
-        file_name_b = 'b' + fast + '.txt'
-        file_name_m = 'm' + fast + '.txt'
-        file_name_ic = 'ic' + fast + '.txt'
-    else:
-        # TODO Perhaps write "Don't include .txt" ???
-        # Detect extension or add if needed
-        filename = input('\nfilename for array c [I will add .txt]=  ')
-        file_name_c = filename + '.txt'
+#input (old inputtest_II_1.py)
+#input data from files
 
-        filename = input('\nfilename for array b [I will add .txt]=  ')
-        file_name_b = filename + '.txt'
+#give it just a number n, will find files cn.txt, bn.txt, mn.txt, icn.txt
+fast=input('\n ONLY NUMBER n and I will find cn.txt, etc. (#/a, Def=a)')
+if fast.isdigit():
+    fnamec='c'+fast+'.txt'
+    fnameb='b'+fast+'.txt'
+    fnamem='m'+fast+'.txt'
+    fnameic='ic'+fast+'.txt'
+else:
+    fname=input('\nfilename for array c [I will add .txt]=  ')
+    fnamec=fname+'.txt'
+    
+    fname=input('\nfilename for array b [I will add .txt]=  ')
+    fnameb=fname+'.txt'
+    
+    fname=input('\nfilename for array m [I will add .txt]=  ')
+    fnamem=fname+'.txt'
+    
+    fname=input('\nfilename for array IC [I will add .txt]=  ')
+    fnameic=fname+'.txt'
 
-        filename = input('\nfilename for array m [I will add .txt]=  ')
-        file_name_m = filename + '.txt'
+#get the files
+c,numc=getxn(fnamec)
+b,numb=getx(fnameb)
+m,numm=getx(fnamem)
+ic,numic=getx(fnameic)
 
-        filename = input('\nfilename for array IC [I will add .txt]=  ')
-        file_name_ic = filename + '.txt'
+#check for consistentcy
+if (numc**3!=numb*numm*numic):
+    print ("\nFATAL WARNING - input issue - numbers c,b,m,ic don't match")
 
-    return file_name_b, file_name_c, file_name_ic, file_name_m
+#make arrays (NOT matrices) and print
+ma=np.array(m)
+ba=np.array(b)
+ca=np.array(c)
+ica=np.array(ic)
+print ('\nca= ',ca)
+print ('\nba= ',ba)
+print ('\nma= ',ma)
+print ('\nic= ',ica)
 
+#xinit=[.0*i for i in range (p)]
+#initial conditions
+# for i in range (p):
+#     xinit[i]=float(input('\ninitial value of x (e.g. .5 or -.5)  '))
+# xold=xinit
+# xolda=np.array(xold)
 
-def print_file_arrays(ca, ba, ma, ica):
-    print('\nca = ', ca)
-    print('\nba = ', ba)
-    print('\nma = ', ma)
-    print('\nic = ', ica)
+#You can change here, but program MUST finish before you get graph
+change=input('\nWant to CHANGE parameters (y/n, def=n)')
+if (change=='y' or change=='Y'):
+    c=lslin('c',c)
+    b=lslin('b',b)
+    m=lslin('m',m)
+    ic=lslin('ic',ic)
+    ma=np.array(m)
+    ba=np.array(b)
+    ca=np.array(c)
+    ica=np.array(ic)
+    print ('/n/nNEW PARAMTER VALUES ARE:')
+    print ('\nca= ',ca)
+    print ('\nba= ',ba)
+    print ('\nma= ',ma)
+    print ('\nic= ',ica)
+    paramin=input('\nNOTE changes here!  ')
+else:
+    pass
+#OK now start the integration
+#fix +/- RESERVOIRS NEVER <0
+ica[1]=max(ica[1],0.)
+ica[2]=max(ica[2],0.)
+xolda=ica
+tt=0
+t=[0. for i in range(numdata)]
+z=np.array([ica for i in range (numdata)]) #NOTE: changed ic to ica HEREß
 
+for i in range (1,numdata):
+    mtanh=np.tanh(z[i-1])
+    cterm=np.dot(ca,mtanh)
+    dx=dt*(ma*z[i-1] + ba + cterm)
+#    print ('\nz[i-1]',z[i-1])
+#    print ('\nma*z[i-1],ba, ca * mtanh, dx','\n',ma*z[i-1],ba, ca * mtanh, dx,'\n\n')
+    tt=tt+dt
+    t[i]=tt
+    z[i]=z[i-1]+dx
+    z[i][1]=max(z[i][1],0.0) # the + reservoir is NEVER negative
+    z[i][2]=max(z[i][2],0.0) # the - reservoir is NEVER negative
+    z[i][3]=max(z[i][3],0.0) # the + future is NEVER negative
+    z[i][4]=max(z[i][4],0.0) # the - future is NEVER negative
+    
+    
+#PLOT
+print('\nYour plot is ready')
+localtime = time.asctime( time.localtime(time.time()) )
+x_start=ica
+x_final=z[-1]
+plt.figure(1)
+#plt.axes([.1,.1,.8,.7])  ORIGINAL
+plt.axes([0.1,.075,.8,.7])
+#new
 
-def get_plots(ba, ca, delta_time, ica, ma, numdata):
-    """Euler inegration"""
-    # OK now start the integration
-    # fix +/- RESERVOIRS NEVER < 0
-    ica[1] = max(ica[1], 0.)
-    ica[2] = max(ica[2], 0.)
-    xolda = ica
-    tt = 0
-    t = [0. for i in range(numdata)]
-    z = np.array([ica for i in range(numdata)])  # NOTE: changed ic to ica HEREß
+plt.plot(t,z[:,0],color='navy',linewidth='6')
+plt.plot(t,z[:,1],color='greenyellow',linewidth='4')
+plt.plot(t,z[:,2],color='hotpink',linewidth='4')
+plt.plot(t,z[:,3:numc])
+#print labels on lines
+xtext=25
+for i in range (numc):
+    ytext=z[-1,i]
+    varis=str(i+1)
+    plt.text(xtext,ytext,varis)
+    xtext=xtext-1
+    
+#plt.text(5,0,'num')
 
-    for i in range(1, numdata):
-        mtanh = np.tanh(z[i - 1])
-        cterm = np.dot(ca, mtanh)
-        dx = delta_time * (ma * z[i - 1] + ba + cterm)
-        #    print ('\nz[i-1]',z[i-1])
-        #    print ('\nma*z[i-1],ba, ca * mtanh, dx','\n',ma*z[i-1],ba, ca * mtanh, dx,'\n\n')
-        tt += delta_time
-        t[i] = tt
-        z[i] = z[i - 1] + dx
-        z[i][1] = max(z[i][1], 0.0)  # the + reservoir is NEVER negative
-        z[i][2] = max(z[i][2], 0.0)  # the - reservoir is NEVER negative
-        z[i][3] = max(z[i][3], 0.0)  # the + future is NEVER negative
-        z[i][4] = max(z[i][4], 0.0)  # the - future is NEVER negative
-        # TODO animate circles here
-    return t, z
+#old
+# #plt.axis([0, 3, -20, 20])
+# lines=plt.plot(t,z)
+# #lines=plt.plot (ta,x1a,'-b',ta,x2a,'-r')
+# plt.setp(lines,linewidth=2.)
+# #plt.show()
+# #plt.setp(lines,linewidth=2.,mec='r')
 
+programname='map2_6boxes.py   '+localtime
+param1='\n   input files= '+str(fnamec)+'    '+str(fnameb)+\
+    '    '+str(fnamem) + '    '+str(fnameic)
+param2='\nx_start= '+str(ica)+'    dt= '+str(dt)+ \
+    '    var colors=ngp-bgrcmyk' + '\nx_final='+str(x_final)
+param4='\n'+paramin
 
-def animate(i):
-    # modify frame step
-    i *= step
+#making it possible to print c matrix
+# nh=int(numc/2)
+# ca1=ca[0:nh,:]
+# ca2=ca[nh:numc,:]
+# ca1s=str(ca1)
+# ca2s=str(ca2)
+# ca1s2=ca1s.replace('\n','')
+# ca2s2=ca2s.replace('\n','')
+# param3='\nb= '+ str(b) +' m= '+str(m) + '\nc= '+ ca1s2 +'\n'+ ca2s2
+titlelsl=programname+param1 + param4 + param2
+plt.title(titlelsl, fontsize=10)
+#plt.axis([0, .1, -.2, .2])
 
-    # get plots for current i value
-    x_plot1, y_plot1 = (t[:i], z[:i, 0])
-    x_plot2, y_plot2 = (t[:i], z[:i, 1])
-    x_plot3, y_plot3 = (t[:i], z[:i, 2])
-    x_plot4, y_plot4 = (t[:i], z[:i, 3:numc])
-
-    # clear lines
-    ax1.clear()
-
-    # replot
-    ax1.plot(x_plot1, y_plot1, color='navy', linewidth=6)
-    ax1.plot(x_plot2, y_plot2, color='greenyellow', linewidth=4)
-    ax1.plot(x_plot3, y_plot3, color='hotpink', linewidth=4)
-    ax1.plot(x_plot4, y_plot4)
-
-    # set axes scale
-    ax1.axis([0.0, 30.0, 0.0, 12.0])
-
-    return ax1
-
-
-tt = 0.
-step = 50
-numdata = 30000
-delta_time = .001
-param_in = 'used data from files'
-
-file_name_b, file_name_c, file_name_ic, file_name_m = generate_filenames()  # get the files
-
-c, numc = getxn(file_name_c)
-b, numb = getx(file_name_b)
-m, numm = getx(file_name_m)
-ic, numic = getx(file_name_ic)
-
-# check for consistency
-if numc ** 3 != numb * numm * numic:
-    print("\nFATAL WARNING - input issue - numbers c, b, m, ic don't match")
-
-# make arrays (NOT matrices) and print
-ca, ba, ma, ica = np.array(c), np.array(b), np.array(m), np.array(ic)
-
-print_file_arrays(ca, ba, ma, ica)
-
-change = input('\nWant to CHANGE parameters (y / n, def = n)')
-
-if change == 'y' or change == 'Y':
-    c, b, m, ic = lslin('c', c), lslin('b', b), lslin('m', m), lslin('ic', ic)
-    ca, ba, ma, ica = np.array(c), np.array(b), np.array(m), np.array(ic)
-    print('/n/nNEW PARAMETER VALUES ARE: ')
-    print_file_arrays(ca, ba, ma, ica)
-    param_in = input('\nNOTE changes here! ')
-
-t, z = get_plots(ba, ca, delta_time, ica, ma, numdata)
-print("plot is ready")
-
-figure = plt.figure()
-ax1 = figure.add_subplot(111)
-
-# init animation
-frames = numdata / step
-anim = animation.FuncAnimation(figure, animate, frames=int(frames), interval=1, blit=False, repeat=False)
-
-# resize graph to fit title
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0 - box.height * 0.05, box.width, box.height * 0.9])
-
-# generate title
-x_start = ica
-x_final = z[-1]
-localtime = time.asctime(time.localtime(time.time()))
-program_name = 'map2_6boxes.py   ' + localtime
-param1 = '\n   input files =  {}    {}    {}    {}'.format(file_name_c, file_name_b, file_name_m, file_name_ic)
-param2 = '\nx_start = {}    delta_time = {}    var colors=ngp-bgrcmyk\nx_final={}' \
-    .format(str(ica), str(delta_time), str(x_final))
-param4 = '\n' + param_in
-title = program_name + param1 + param4 + param2
-
-# set title and show graph
-plt.suptitle(title, fontsize=10)
-# plt.show()
-boxplot(ca, x_final, program_name)
+#OK, now trying to print the second plot
+zzin=x_final
+ccin=ca
+pprogamename=programname
+boxplot(ccin,zzin,pprogamename)
